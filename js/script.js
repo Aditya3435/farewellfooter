d5f4e99a192f0a066aa36875c9ea929a65958cde
Aditya Maurya <adityamaurya3435@gmail.com>
2022-05-08 16:35:32 +0530

    gradient-bg

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const text = document.getElementById('textrotate');
-text.innerHTML = text.innerText.split("").map(
-    (char, i) =>
-    `<span style="transform:rotate(${i*16}deg)">${char}</span>`
-).join("");
